801c64761dd4e02ff1e0a63a530e4338567332e8
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-01-01 22:49:04 +0530

    Create factorial.cpp

---
+++ cc/factorial.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int tcase;
+    cin>>tcase;
+    for(int i=0;i<tcase;i++){
+        int num,fact=1;
+        cin>>num;
+    if(num==1&&num==0){
+            cout<<"1\n";
+    }
+    else{
+        for(int j=1;j<=num;++j){
+            fact*=j;
+        }
+    }
+        cout<<fact<<"\n";
+
+    }
+	return 0;
+}
