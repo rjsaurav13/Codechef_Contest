3e47a36e4dcfbb299bc7720061856ab20c4149e8
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-01-18 14:55:50 +0530

    Update Sqroot.cpp

--- cc/Sqroot.cpp
+++ cc/Sqroot.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num,temp,tcase;
 
+    cin>>tcase;
+    for(int k=0;k<tcase;k++){
+        cin>>num;
+        for(int i=1;i<=100;i++){
+            int j=i*i;
+            if(num>j-1||num==j){
+                temp=i;
+
+            }
+        }
+        cout<<temp<<"\n";
+    }
+return 0;
+}
