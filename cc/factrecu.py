f79bf1ddd05540afe3738336c095d34f2793befb
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-01-19 00:26:47 +0530

    Create factrecu.py

---
+++ cc/factrecu.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: saurav
+"""
+
+def fact(num):
+   if num == 1:
+       return num
+   else:
+       return num*fact(num-1)
+tcase=int(input())
+for i in range(0, tcase):
+    num=int(input())
+    print(fact(num))
