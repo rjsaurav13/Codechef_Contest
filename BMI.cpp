f9ec99a81da7d43845b554ae870471094f2a0b8b
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-02-24 22:19:56 +0530

    Create BMI.cpp

---
+++ BMI.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+
+        int result=29;
+        if(result<=18){
+            printf("1\n");
+        }
+        else if(result>18 && result<25){
+            printf("2\n");
+        }
+        else if(result>24 && result<30){
+            printf("3\n");
+        }
+        else if(result>=30){
+            printf("4\n");
+        }
+
+    return 0;
+}
